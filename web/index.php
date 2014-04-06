8a7ec58c588201c23f48d291a497198538ca5372
Olivier Revollat <revollat@gmail.com>
2014-04-06 23:49:51 +0200

    initialisation source pour micro framework

--- web/index.php
+++ web/index.php
@@ -1,13 +1,5 @@
 <?php
-use Symfony\Component\HttpFoundation\Response;
 
-require_once __DIR__.'/../vendor/autoload.php';
+$input = $_GET['name'];
 
-// Twig template engine
-$twig = new Twig_Environment(new Twig_Loader_Filesystem(array(
-    realpath(__DIR__ . '/../views'),
-)));
-
-$response = new Response($twig->render('test.html.twig', array('toto' => 'essai')));
-
-$response->send();
+printf('Hello %s', $input);
