687432a01b45be133cd6f8cf8542d4fdcb839288
Olivier Revollat <revollat@gmail.com>
2014-04-07 00:12:27 +0200

    mf-1.3

--- web/index.php
+++ web/index.php
@@ -1,10 +1,14 @@
 <?php
 
-// 1 -
-// Il faut sécuriser l'application pour empecher les attaques de type XSS
+require_once __DIR__.'/../vendor/autoload.php';
 
-$input = isset($_GET['name']) ? $_GET['name'] : 'World';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-header('Content-Type: text/html; charset=utf-8');
+$request = Request::createFromGlobals();
 
-printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
