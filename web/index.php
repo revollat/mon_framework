71c1feb384229b93ab32fcc9129e322574ed5b84
Olivier Revollat <revollat@gmail.com>
2014-04-06 20:50:50 +0200

    utilisation de twig

--- web/index.php
+++ web/index.php
@@ -1,3 +1,13 @@
 <?php
-echo date("Ymd");
-echo "test";
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+// Twig template engine
+$twig = new Twig_Environment(new Twig_Loader_Filesystem(array(
+    realpath(__DIR__ . '/../views'),
+)));
+
+$response = new Response($twig->render('test.html.twig', array('toto' => 'essai')));
+
+$response->send();
