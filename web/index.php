72446f8d5c2cfcab8b9d888595f48d37e9ac987a
Olivier Revollat <revollat@gmail.com>
2014-04-07 00:04:18 +0200

    mf-1.1

--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
 
-$input = $_GET['name'];
+// 1-
+// Pour eviter le message d'erreur si la variable n'est pas dans l'URL en GET.
+
+$input = isset($_GET['name']) ? $_GET['name'] : 'World';
 
 printf('Hello %s', $input);
