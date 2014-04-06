665b5efad1d3b5ad3cf2a36e092697879445eba2
Vagrant User <vagrant@packer-virtualbox.vagrantup.com>
2014-04-06 22:57:56 +0000

    mf-2.4

--- web/front.php
+++ web/front.php
@@ -1,8 +1,6 @@
 <?php
 
-// VOIR CONF NGINX :
-// /etc/nginx/conf.d/vhost_autogen.conf
-// pour faire de front.php le "Front controller"
+// example.com/web/front.php
 
 require_once __DIR__.'/../vendor/autoload.php';
 
@@ -13,13 +11,15 @@ $request = Request::createFromGlobals();
 $response = new Response();
 
 $map = array(
-    '/hello' => __DIR__.'/hello.php',
-    '/bye'   => __DIR__.'/bye.php',
+    '/hello' => __DIR__.'/../src/pages/hello.php',
+    '/bye'   => __DIR__.'/../src/pages/bye.php',
 );
 
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
-    require $map[$path];
+    ob_start();
+    include $map[$path];
+    $response->setContent(ob_get_clean());
 } else {
     $response->setStatusCode(404);
     $response->setContent('Not Found');
