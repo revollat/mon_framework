9faefa0ce719286586765590eae5937b9c114d66
Olivier Revollat <revollat@gmail.com>
2014-04-09 15:38:39 +0200

    Silex fresh install

--- web/front.php
+++ web/front.php
@@ -1,31 +1,11 @@
 <?php
-
-// example.com/web/front.php
-
+// web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
+$app = new Silex\Application();
 
-function render_template(Request $request)
-{
-    extract($request->attributes->all());
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+$app->get('/hello/{name}', function ($name) use ($app) {
+return 'Hello '.$app->escape($name);
+});
 
-    return new Response(ob_get_clean());
-}
-
-$request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
-
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$framework = new Simplex\Framework($matcher, $resolver);
-$response = $framework->handle($request);
-
-$response->send();
+$app->run();
