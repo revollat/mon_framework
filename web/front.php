0242789fe8630afcc7891db61ed5d2be72e1519c
Olivier Revollat <revollat@gmail.com>
2014-04-09 17:22:23 +0200

    ajout twig provider pour silex et création layout avec le framework css skeleton

--- web/front.php
+++ web/front.php
@@ -2,12 +2,20 @@
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
+// Instantiation d'une nouvelle application Silex !! :)
 $app = new Silex\Application();
 
+// On afiche les erreurs (car on est en train de développer le site)
 $app['debug'] = true;
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-return 'Hello '.$app->escape($name);
+// On initialise TWIG en lui indiquant le dossier ou se trouve les templates.
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../templates',
+));
+
+// ACCUEIL
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('pages/accueil.html.twig');
 });
 
 $app->run();
